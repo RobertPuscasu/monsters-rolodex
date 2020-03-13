fedd6201b4a71385dc8ce71d388b61f1d6ee34d0
Robert Puscasu <robert.puscasu@tradeshift.com>
2020-03-13 13:08:01 +0200

    Updates

---
+++ precache-manifest.a1f2e29bd84ce09ff1a16a8a83232ad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5897778d203d911858feab31ca3ce9c",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a0ebfbf32f7872b259d9",
+    "url": "/monsters-rolodex/static/css/main.e0c117e0.chunk.css"
+  },
+  {
+    "revision": "cb0ccc0f2b23e601e293",
+    "url": "/monsters-rolodex/static/js/2.1f66fe0d.chunk.js"
+  },
+  {
+    "revision": "e468b20662ce7acec401182a9064b329",
+    "url": "/monsters-rolodex/static/js/2.1f66fe0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0ebfbf32f7872b259d9",
+    "url": "/monsters-rolodex/static/js/main.9e430d77.chunk.js"
+  },
+  {
+    "revision": "bf0efc50803a2c0daead",
+    "url": "/monsters-rolodex/static/js/runtime-main.e47bae1e.js"
+  }
+]);
